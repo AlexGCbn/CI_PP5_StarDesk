d25a5b3e5f3fa82fd227407c830813a43d39d0f2
Alex Ciobanu <tsopi10@gmail.com>
2022-05-18 11:49:18 +0000

    Add home app and template

---
+++ home/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.views import generic, View
+
+
+class IndexView(View):
+    """ Basic index view """
+
+    def get(self, request):
+        """ A view to return index page """
+        template = 'home/index.html'
+        return render(request, template)

e3742f5fff6988a79cdc4bb7cd95b9afe44bb4c3
Alex Ciobanu <tsopi10@gmail.com>
2022-05-26 16:27:18 +0000

    Style products page

--- products/views.py
+++ products/views.py
@@ -8,31 +8,32 @@ class AllProductsView(View):
 
     def get(self, request):
         """ A view to return index page """
-        # values_list info from here https://books.agiliq.com/projects/django-orm-cookbook/en/latest/union.html
-        query_case = Case.objects.all().values_list(
-            'model', 'manufacturer', 'price', 'rating'
+        # values info from here https://books.agiliq.com/projects/django-orm-cookbook/en/latest/union.html
+        query_case = Case.objects.all().only(
+            'model', 'manufacturer', 'price', 'rating', 'image'
         )
-        query_mobo = Motherboard.objects.all().values_list(
-            'model', 'manufacturer', 'price', 'rating'
+        query_mobo = Motherboard.objects.all().only(
+            'model', 'manufacturer', 'price', 'rating', 'image'
         )
-        query_cpu = Cpu.objects.all().values_list(
-            'model', 'manufacturer', 'price', 'rating'
+        query_cpu = Cpu.objects.all().only(
+            'model', 'manufacturer', 'price', 'rating', 'image'
         )
-        query_gpu = Gpu.objects.all().values_list(
-            'model', 'manufacturer', 'price', 'rating'
+        # query_cpu = Cpu.objects.all()
+        query_gpu = Gpu.objects.all().only(
+            'model', 'manufacturer', 'price', 'rating', 'image'
         )
-        query_ram = Ram.objects.all().values_list(
-            'model', 'manufacturer', 'price', 'rating'
+        query_ram = Ram.objects.all().only(
+            'model', 'manufacturer', 'price', 'rating', 'image'
         )
-        query_psu = Psu.objects.all().values_list(
-            'model', 'manufacturer', 'price', 'rating'
+        query_psu = Psu.objects.all().only(
+            'model', 'manufacturer', 'price', 'rating', 'image'
         )
-        query_storage = Storage.objects.all().values_list(
-            'model', 'manufacturer', 'price', 'rating'
+        query_storage = Storage.objects.all().only(
+            'model', 'manufacturer', 'price', 'rating', 'image'
         )
         products = query_case.union(
             query_mobo,
-            query_cpu,
+            # query_cpu,
             query_gpu,
             query_ram,
             query_psu,
@@ -41,5 +42,6 @@ class AllProductsView(View):
         template = 'products/products.html'
         context = {
             'products': products,
+            'cpus': query_cpu
         }
         return render(request, template, context)

900fb49d81331c7deb06d48d2a66f7684c71ded7
Alex Ciobanu <tsopi10@gmail.com>
2022-05-26 11:49:27 +0000

    Add products view and url

--- products/views.py
+++ products/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from django.views import generic, View
+from .models import Case, Motherboard, Cpu, Gpu, Ram, Psu, Storage
 
-# Create your views here.
+
+class AllProductsView(View):
+    """ View to display all products """
+
+    def get(self, request):
+        """ A view to return index page """
+        query_case = Case.objects.all()
+        query_mobo = Motherboard.objects.all()
+        query_cpu = Cpu.objects.all()
+        query_gpu = Gpu.objects.all()
+        query_ram = Ram.objects.all()
+        query_psu = Psu.objects.all()
+        query_storage = Storage.objects.all()
+        products = query_case.union(
+            query_mobo,
+            query_cpu,
+            query_gpu,
+            query_ram,
+            query_psu,
+            query_storage
+        )
+        template = 'products/products.html'
+        context = {
+            'products': products,
+        }
+        return render(request, template, context)

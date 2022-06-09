55fd5b1df4c5ded9c104c976eec07ce3ee450576
Alex Ciobanu <tsopi10@gmail.com>
2022-06-09 14:07:48 +0000

    Add bag product update quantity functionality

--- bag/views.py
+++ bag/views.py
@@ -10,3 +10,20 @@ class BagView(View):
 
         template = 'bag/bag.html'
         return render(request, template)
+
+    # Temporary
+    def post(self, request):
+        """ POST request for updating/removing Bag items """
+
+        
+        redirect_url = request.POST.get('redirect_url')
+        bag = request.session.get('bag', {})
+        item = request.POST.get('product_pass')
+
+        if request.POST.get('operation') == 'update':
+            quantity = int(request.POST.get('value'))
+            bag[item] = quantity
+
+
+        request.session['bag'] = bag
+        return redirect(redirect_url)

e9b56fc63a37648f34911c3c025fe63fcd360778
Alex Ciobanu <tsopi10@gmail.com>
2022-06-16 14:37:41 +0000

    Add basic checkout functionality

--- checkout/views.py
+++ checkout/views.py
@@ -4,12 +4,17 @@ from django.views import View
 from django.contrib import messages
 from .forms import OrderForm
 from bag.contexts import bag_contents
+import stripe
 
 
 class CheckoutView(View):
     """
     Checkout class view
     """
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+    stripe.api_key = stripe_secret_key
+
     def get(self, request):
         """
         GET request view for checkout
@@ -22,11 +27,22 @@ class CheckoutView(View):
         current_bag = bag_contents(request)
         total = current_bag['grand_total']
         stripe_total = round(total * 100)
+
+        intent = stripe.PaymentIntent.create(
+            amount=stripe_total,
+            currency=settings.STRIPE_CURRENCY,
+        )
+
+        if not self.stripe_public_key:
+            messages.warning(request, 'Stripe public key is missing. \
+                Did you set it up in your environment?')
+
         order_form = OrderForm()
         template = 'checkout/checkout.html'
         context = {
             'order_form': order_form,
-            'stripe_public_key': settings.STRIPE_PUBLIC_KEY,
+            'stripe_public_key': self.stripe_public_key,
+            'client_secret': intent.client_secret,
         }
 
         return render(request, template, context)

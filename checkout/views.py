e5acc08d12a911e6c8fe80507dc896e1bc2f1252
Alex Ciobanu <tsopi10@gmail.com>
2022-06-16 14:22:49 +0000

    Add stripe basic functionality

--- checkout/views.py
+++ checkout/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, reverse, redirect
+from django.conf import settings
 from django.views import View
 from django.contrib import messages
 from .forms import OrderForm
+from bag.contexts import bag_contents
 
 
 class CheckoutView(View):
@@ -17,10 +19,14 @@ class CheckoutView(View):
             messages.error(request, 'Nothing found in bag. Please add some items.')
             return redirect(reverse('products'))
         
+        current_bag = bag_contents(request)
+        total = current_bag['grand_total']
+        stripe_total = round(total * 100)
         order_form = OrderForm()
         template = 'checkout/checkout.html'
         context = {
             'order_form': order_form,
+            'stripe_public_key': settings.STRIPE_PUBLIC_KEY,
         }
 
         return render(request, template, context)

1bcdc07d6434140fb355d350d6d0cff1099ffbb7
Alex Ciobanu <tsopi10@gmail.com>
2022-06-20 11:25:26 +0000

    Add new superuser, delete old ones, ensure login is working

--- profiles/models.py
+++ profiles/models.py
@@ -10,7 +10,7 @@ class UserProfile(models.Model):
     """
     User profile model to save profile information
     """
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     profile_full_name = models.CharField(max_length=50, null=True, blank=True)
     profile_phone_number = models.CharField(max_length=20, null=True, blank=True)
     profile_country = CountryField(blank_label='Country *', null=True, blank=True)
@@ -20,7 +20,7 @@ class UserProfile(models.Model):
     profile_street_address2 = models.CharField(max_length=80, null=True, blank=True)
 
     def __str__(self):
-        return f'{self.user.username} - {self.profile_full_name}'
+        return self.user.username
 
 
 @receiver(post_save, sender=User)

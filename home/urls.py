faad7a2978d49261394d286e437b1ee65e4a6402
Alex Ciobanu <tsopi10@gmail.com>
2022-06-26 09:56:13 +0000

    Add style rules to keep page size correct

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
+from django.contrib.auth.decorators import login_required
 from . import views
 
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='home'),
-    path('add_deal/<str:category>/<int:id>/', views.AdminAddDeal.as_view(), name='add_deal'),
+    path('add_deal/<str:category>/<int:id>/', login_required(views.AdminAddDeal.as_view()), name='add_deal'),
 ]

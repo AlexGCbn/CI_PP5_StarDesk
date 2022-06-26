7da8d0981ec77bf401b4b73953580a8da128b0a7
Alex Ciobanu <tsopi10@gmail.com>
2022-06-26 21:16:33 +0000

    Test product views - 100%

--- products/test_views.py
+++ products/test_views.py
@@ -6,6 +6,11 @@ from .models import (
     Cpu, Gpu, Ram,
     Psu, Storage
 )
+from .forms import (
+    CaseForm, MotherboardForm,
+    CpuForm, GpuForm, RamForm,
+    PsuForm, StorageForm
+)
 from reviews.models import CaseReview
 import datetime
 
@@ -82,6 +87,12 @@ class TestViews(TestCase):
             speed=3500,
             image=tempfile.NamedTemporaryFile(suffix=".jpg").name
         )
+        self.normal_user = User.objects.create(username='testuser', email='test@email.com')
+        self.normal_user.set_password('test_password')
+        self.normal_user.save()
+        self.admin = User.objects.create_superuser(username='testadmin', email='test@email.com')
+        self.admin.set_password('test_password')
+        self.admin.save()
 
     def test_get_all_products(self):
         """
@@ -164,18 +175,15 @@ class TestViews(TestCase):
 
     def test_product_reviewed(self):
         """Test product reviewed by user"""
-        user = User.objects.create(username='testuser')
-        user.set_password('test_password')
-        user.save()
-        client = Client()
+        user = self.normal_user
         self.test_review = CaseReview.objects.create(
             product=self.test_case,
             user=user,
             score=4,
             comment='test_comment',
         )
-        client.login(username='testuser', password='test_password')
-        response = client.get('/products/case/1/')
+        self.client.login(username='testuser', password='test_password')
+        response = self.client.get('/products/case/1/')
         self.assertEqual(response.status_code, 200)
 
     def test_add_product_to_bag(self):
@@ -190,13 +198,7 @@ class TestViews(TestCase):
         self.assertRedirects(response, '/products/motherboard/1/')
 
     def test_admin_add_product_get(self):
-        """Test admin add product GET"""
-        normal_user = User.objects.create(username='testuser', email='test@email.com')
-        normal_user.set_password('test_password')
-        normal_user.save()
-        admin = User.objects.create_superuser(username='testadmin', email='test@email.com')
-        admin.set_password('test_password')
-        admin.save()
+        """Test admin add product GET request"""
         self.client.login(username='testadmin', password='test_password')
         response = self.client.get('/products/add_product/', follow=True)
         self.assertEqual(response.status_code, 200)
@@ -206,4 +208,270 @@ class TestViews(TestCase):
         response = self.client.get('/products/add_product/', follow=True)
         self.assertRedirects(response, '/products/')
 
+    def test_admin_add_case_post(self):
+        """Test admin add case POST request"""
+        self.client.login(username='testadmin', password='test_password')
+        response = self.client.post('/products/add_product/', {
+            'category': 'case',
+            'model': 'test_model2',
+            'manufacturer': 'test_manufacturer2',
+            'description': 'test_description2',
+            'price': 123,
+            'mobo_sizes': 'atx',
+        }, follow=True)
+        
+        new_product = Case.objects.get(id=2)
+        self.assertEqual(new_product.model, 'test_model2')
 
+        response = self.client.post('/products/add_product/', {
+            'category': 'case',
+            'model': 'test_model2',
+            'manufacturer': 'test_manufacturer2',
+            'description': 'test_description2',
+            'price': 123,
+        }, follow=True)
+        self.assertRedirects(response, '/products/add_product/')
+
+        self.client.logout()
+        self.client.login(username='testuser', password='test_password')
+        response = self.client.post('/products/add_product/', {
+            'category': 'case',
+            'model': 'test_model2',
+            'manufacturer': 'test_manufacturer2',
+            'description': 'test_description2',
+            'price': 123,
+            'mobo_sizes': 'atx',
+        }, follow=True)
+        self.assertRedirects(response, '/products/')
+
+    def test_admin_add_rest_of_products(self):
+        """Test admin add product POST request"""
+        self.client.login(username='testadmin', password='test_password')
+        self.client.post('/products/add_product/', {
+            'category': 'motherboard',
+            'model': 'test_model2',
+            'manufacturer': 'test_manufacturer2',
+            'description': 'test_description2',
+            'price': 123,
+            'size': 'micro_atx',
+            'socket': '1200',
+            'ram_type': 'ddr4',
+        }, follow=True)
+        new_product = Motherboard.objects.get(id=2)
+        self.assertEqual(new_product.model, 'test_model2')
+
+        response = self.client.post('/products/add_product/', {
+            'category': 'cpu',
+            'model': 'test_model2',
+            'manufacturer': 'test_manufacturer2',
+            'description': 'test_description2',
+            'price': 123,
+            'socket': '1200',
+            'core_count': 8,
+        }, follow=True)
+        new_product = Cpu.objects.get(id=2)
+        self.assertEqual(new_product.model, 'test_model2')
+
+        response = self.client.post('/products/add_product/', {
+            'category': 'gpu',
+            'model': 'test_model2',
+            'manufacturer': 'test_manufacturer2',
+            'description': 'test_description2',
+            'price': 123,
+            'speed': '2200',
+            'memory_capacity': 8,
+        }, follow=True)
+        new_product = Gpu.objects.get(id=2)
+        self.assertEqual(new_product.model, 'test_model2')
+
+        response = self.client.post('/products/add_product/', {
+            'category': 'ram',
+            'model': 'test_model2',
+            'manufacturer': 'test_manufacturer2',
+            'description': 'test_description2',
+            'price': 123,
+            'speed': 3200,
+            'capacity': 16,
+            'type': 'ddr4',
+        }, follow=True)
+        new_product = Ram.objects.get(id=2)
+        self.assertEqual(new_product.model, 'test_model2')
+
+        response = self.client.post('/products/add_product/', {
+            'category': 'psu',
+            'model': 'test_model2',
+            'manufacturer': 'test_manufacturer2',
+            'description': 'test_description2',
+            'price': 123,
+            'wattage': 700,
+            'e_category': '80plus_gold',
+        }, follow=True)
+        new_product = Psu.objects.get(id=2)
+        self.assertEqual(new_product.model, 'test_model2')
+
+        self.client.post('/products/add_product/', {
+            'category': 'storage',
+            'model': 'test_model2',
+            'manufacturer': 'test_manufacturer2',
+            'description': 'test_description2',
+            'price': 123,
+            'capacity': 500,
+            'speed': 3500,
+        }, follow=True)
+        new_product = Storage.objects.get(id=2)
+        self.assertEqual(new_product.model, 'test_model2')
+
+    def test_admin_manage_product_get(self):
+        """Test admin manage case GET request"""
+        self.client.login(username='testadmin', password='test_password')
+        response = self.client.get('/products/case/1/edit/', {'category': 'case', 'id': 1}, follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'products/edit_product.html')
+
+        self.client.logout()
+        self.client.login(username='testuser', password='test_password')
+        response = self.client.get('/products/case/1/edit/', {'category': 'case', 'id': 1}, follow=True)
+        self.assertRedirects(response, '/products/')
+
+    def test_admin_manage_product_get_rest(self):
+        """Test admin manage products GET request"""
+        self.client.login(username='testadmin', password='test_password')
+        response = self.client.get('/products/motherboard/1/edit/', {'category': 'motherboard', 'id': 1}, follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'products/edit_product.html')
+        response = self.client.get('/products/cpu/1/edit/', {'category': 'cpu', 'id': 1}, follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'products/edit_product.html')
+        response = self.client.get('/products/gpu/1/edit/', {'category': 'gpu', 'id': 1}, follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'products/edit_product.html')
+        response = self.client.get('/products/ram/1/edit/', {'category': 'ram', 'id': 1}, follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'products/edit_product.html')
+        response = self.client.get('/products/psu/1/edit/', {'category': 'psu', 'id': 1}, follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'products/edit_product.html')
+        response = self.client.get('/products/storage/1/edit/', {'category': 'storage', 'id': 1}, follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'products/edit_product.html')
+
+    def test_admin_manage_product_post(self):
+        """Test admin manage case POST request"""
+        self.client.login(username='testadmin', password='test_password')
+        response = self.client.post('/products/case/1/edit/', {
+            'category': 'case',
+            'id': 1,
+            'model': 'updated_model',
+            'manufacturer': 'test_manufacturer',
+            'description': 'test_description',
+            'price': 123,
+            'mobo_sizes': 'micro_atx',
+        }, follow=True)
+        self.assertEqual(response.status_code, 200)
+        edited_product = Case.objects.get(id=1)
+        self.assertEqual(edited_product.model, 'updated_model')
+
+        self.client.logout()
+        self.client.login(username='testuser', password='test_password')
+        response = self.client.post('/products/case/1/edit/', {
+            'category': 'case',
+            'id': 1,
+            'model': 'model',
+            'manufacturer': 'test_manufacturer',
+            'description': 'test_description',
+            'price': 123,
+            'mobo_sizes': 'micro_atx',
+        }, follow=True)
+        self.assertRedirects(response, '/products/')
+        edited_product = Case.objects.get(id=1)
+        self.assertEqual(edited_product.model, 'updated_model')
+
+        self.client.logout()
+        self.client.login(username='testadmin', password='test_password')
+        response = self.client.post('/products/case/1/delete/', {'category': 'case', 'id': 1,}, follow=True)
+        cases = Case.objects.all()
+        self.assertQuerysetEqual(cases, [])
+
+    def test_admin_manage_product_post_rest(self):
+        """Test admin manage products POST request"""
+        self.client.login(username='testadmin', password='test_password')
+        response = self.client.post('/products/motherboard/1/edit/', {
+            'category': 'motherboard',
+            'id': 1,
+            'model': 'updated_model',
+            'manufacturer': 'test_manufacturer2',
+            'description': 'test_description2',
+            'price': 123,
+            'size': 'micro_atx',
+            'socket': '1200',
+            'ram_type': 'ddr4',
+        }, follow=True)
+        self.assertEqual(response.status_code, 200)
+        edited_product = Motherboard.objects.get(id=1)
+        self.assertEqual(edited_product.model, 'updated_model')
+        response = self.client.post('/products/cpu/1/edit/', {
+            'category': 'cpu',
+            'id': 1,
+            'model': 'updated_model',
+            'manufacturer': 'test_manufacturer2',
+            'description': 'test_description2',
+            'price': 123,
+            'socket': '1200',
+            'core_count': 8,
+        }, follow=True)
+        self.assertEqual(response.status_code, 200)
+        edited_product = Cpu.objects.get(id=1)
+        self.assertEqual(edited_product.model, 'updated_model')
+        response = self.client.post('/products/gpu/1/edit/', {
+            'category': 'gpu',
+            'id': 1,
+            'model': 'updated_model',
+            'manufacturer': 'test_manufacturer2',
+            'description': 'test_description2',
+            'price': 123,
+            'speed': '2200',
+            'memory_capacity': 8,
+        }, follow=True)
+        self.assertEqual(response.status_code, 200)
+        edited_product = Gpu.objects.get(id=1)
+        self.assertEqual(edited_product.model, 'updated_model')
+        response = self.client.post('/products/ram/1/edit/', {
+            'category': 'ram',
+            'id': 1,
+            'model': 'updated_model',
+            'manufacturer': 'test_manufacturer2',
+            'description': 'test_description2',
+            'price': 123,
+            'speed': 3200,
+            'capacity': 16,
+            'type': 'ddr4',
+        }, follow=True)
+        self.assertEqual(response.status_code, 200)
+        edited_product = Ram.objects.get(id=1)
+        self.assertEqual(edited_product.model, 'updated_model')
+        response = self.client.post('/products/psu/1/edit/', {
+            'category': 'psu',
+            'id': 1,
+            'model': 'updated_model',
+            'manufacturer': 'test_manufacturer2',
+            'description': 'test_description2',
+            'price': 123,
+            'wattage': 700,
+            'e_category': '80plus_gold',
+        }, follow=True)
+        self.assertEqual(response.status_code, 200)
+        edited_product = Psu.objects.get(id=1)
+        self.assertEqual(edited_product.model, 'updated_model')
+        response = self.client.post('/products/storage/1/edit/', {
+            'category': 'storage',
+            'id': 1,
+            'model': 'updated_model',
+            'manufacturer': 'test_manufacturer2',
+            'description': 'test_description2',
+            'price': 123,
+            'capacity': 500,
+            'speed': 3500,
+        }, follow=True)
+        self.assertEqual(response.status_code, 200)
+        edited_product = Storage.objects.get(id=1)
+        self.assertEqual(edited_product.model, 'updated_model')
